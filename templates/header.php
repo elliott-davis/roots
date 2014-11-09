92ff5add27ee57ba0fe15e26e4a9e611a4e039a1
Elliott Davis <elliott@excellent.io>
2014-11-09 11:20:59 -0600

    added new navbar mobile logic

--- templates/header.php
+++ templates/header.php
@@ -1,5 +1,6 @@
 <header class="banner navbar navbar-default navbar-static-top" role="banner">
   <div class="container">
+    <a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>"><img alt="Brand" src="/assets/img/logo.png"></a>
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
         <span class="sr-only">Toggle navigation</span>
@@ -7,7 +8,6 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>"><img alt="Brand" src="/assets/img/logo.png" height="60"></a>
     </div>
 
     <nav class="collapse navbar-collapse navbar-right" role="navigation">
