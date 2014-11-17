226773ef90c03a52417acd431bf36a31bd33dae8
Elliott Davis <elliott@excellent.io>
2014-11-16 18:34:45 -0600

    Removed trackbacks page for livefyre

--- templates/footer.php
+++ templates/footer.php
@@ -5,8 +5,7 @@
         wp_nav_menu(array('theme_location' => 'footer_navigation', 'menu_class' => 'footernav'));
       endif;
     ?>
-    <div class="copyright pull-right">©2013-2014 That Oven Feelin'.
-    <em>Design by</em> <a href="http://excellent.io" target="_blank">Excellent.io</a>.<br>
+    <div class="copyright pull-right">©2013-2014 That Oven Feelin'.<br>
     </div>
   </div>
 </footer>
